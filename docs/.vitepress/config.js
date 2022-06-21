bf91c3aca3140e53efc6d36d1ce86279dfff1bcb
Razzh <136013779@qq.com>
2022-06-21 14:42:03 +0800

    chore: init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+export default {
+  title: 'razzh',
+  description: 'razzh site',
+  base: '/blog/',
+  themeConfig: {
+    nav: [
+      { text: 'Start', link: '/start' },
+      { text: 'Configs', link: '/configs' },
+      { text: 'Changelog', link: 'https://github.com/rzhAvenir/blog' },
+    ],
+  },
+};
