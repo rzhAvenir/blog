5ee4d6a77ce88c2334fde2644edece28fad799fa
Razzh <136013779@qq.com>
2022-07-31 07:52:25 +0800

    chore: the end component color change

--- docs/.vitepress/theme/components/TheEnd.vue
+++ docs/.vitepress/theme/components/TheEnd.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const isDark = ref(false)
+let darkStatus = localStorage.getItem('vitepress-theme-appearance')
+const validateDark = () => {
+    if (darkStatus === 'light') {
+      isDark.value = false
+    } else {
+      isDark.value = true
+    }
+}
+validateDark()
+
+const mutationObserver = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    darkStatus = localStorage.getItem('vitepress-theme-appearance')
+    validateDark()
+  })
+})
+/** Watch Element Node**/
+mutationObserver.observe(window.document.documentElement<HtmlElement>, {
+  attributes: true
+})
+</script>
 
 <template>
-  <img class="end" src="/the-end.svg" />
+  <img v-if="!isDark" class="end" src="/the-end-light.svg" />
+  <img v-if="isDark" class="end" src="/the-end-dark.svg" />
 </template>
 
 <style scoped>
