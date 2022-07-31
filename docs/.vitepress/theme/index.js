7158fbd6cee59e715b0cfae0f5d4b715992b226a
Razzh <136013779@qq.com>
2022-07-31 10:47:16 +0800

    chore: add unocss

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 import VirtualBook from './components/VirtualBook.vue'
 import TheEnd from './components/TheEnd.vue'
 import './styles/index.css'
+import 'uno.css'
 
 export default {
   ...DefaultTheme,
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import DefineOptions from 'unplugin-vue-define-options/vite'
+import UnoCss from 'unocss/vite'
+import { presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig(() => {
   console.log('有执行吗')
   return {
-    plugins: [DefineOptions()]
+    plugins: [DefineOptions(), UnoCss({
+      presets: [
+        presetAttributify(),
+        presetUno(),
+      ]
+    })]
   }
 })
